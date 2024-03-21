15f83cfb4ad79abf538a4f3dd35fdc50919eeebc
ES, Choi <69956156+escape1001@users.noreply.github.com>
2024-03-21 17:22:03 +0900

    "update" archive에 like 모델 추가

--- archive/admin.py
+++ archive/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Country, City, District, Post, Category, Member
+from .models import Country, City, District, Post, Category, Member, Like
 
 admin.site.register(Country)
 admin.site.register(City)
@@ -7,3 +7,4 @@ admin.site.register(District)
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Member)
+admin.site.register(Like)
--- archive/models.py
+++ archive/models.py
@@ -60,3 +60,12 @@ class Member(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} > {self.post.title}"
